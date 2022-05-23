72db76f31bbe0d49c9cd0e6f0e42299f1500abee
youngjeJO <yj01591@gmail.com>
2022-05-23 19:27:43 +0900

    learn as, attrs

--- src/App.js
+++ src/App.js
@@ -4,30 +4,14 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Sun = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Text = styled.span`
-  color: white;
-`;
-
-const Circle = styled(Sun)`
-  border-radius: 50px;
+const Input = styled.input.attrs({ required: true, maxLength: 10 })`
+  background-color: blue;
 `;
 
 function App() {
   return (
-    <Father>
-      <Sun bgColor='teal'>
-        <Text>hello</Text>
-      </Sun>
-      <Sun bgColor='tomato'>
-        <Text>world</Text>
-      </Sun>
-      <Circle bgColor='gray'></Circle>
+    <Father as='header'>
+      <Input />
     </Father>
   );
 }
