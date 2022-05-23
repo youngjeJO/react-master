bc858fd4772bc3a767b996b09a6020d46b4d64cd
youngjeJO <yj01591@gmail.com>
2022-05-23 19:06:02 +0900

    learn props, extend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import styled from 'styled-components';
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Sun = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
+
+const Circle = styled(Sun)`
+  border-radius: 50px;
+`;
+
 function App() {
-  return null;
+  return (
+    <Father>
+      <Sun bgColor='teal'>
+        <Text>hello</Text>
+      </Sun>
+      <Sun bgColor='tomato'>
+        <Text>world</Text>
+      </Sun>
+      <Circle bgColor='gray'></Circle>
+    </Father>
+  );
 }
 
 export default App;
