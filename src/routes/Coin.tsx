d6dae859b7f1dc852694bcd6ad0a235e9476e514
youngjeJO <yj01591@gmail.com>
2022-05-26 22:03:21 +0900

    feature tab, nested routes

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { Link, Route, Routes, useLocation, useMatch, useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import Chart from './Chart';
+import Price from './Price';
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -26,6 +28,49 @@ const Loader = styled.div`
   text-align: center;
   font-size: 24px;
   color: ${(props) => props.theme.textColor};
+`;
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) => (props.isActive ? props.theme.accentColor : props.theme.textColor)};
+  a {
+    display: block;
+  }
 `;
 
 interface StateInterface {
@@ -93,7 +138,10 @@ function Coin() {
   const state = useLocation().state as StateInterface;
   const [info, setInfo] = useState<InformaionData>();
   const [price, setPrice] = useState<PriceData>();
-
+  const priceMatch = useMatch('/:coinId/price');
+  const chartMatch = useMatch('/:coinId/chart');
+  console.log(priceMatch);
+  console.log(chartMatch);
   useEffect(() => {
     (async () => {
       const informaionData = await (
@@ -108,14 +156,57 @@ function Coin() {
       setPrice(priceData);
       setLoading(false);
     })();
-  }, []);
+  }, [coinId]);
 
   return (
     <Container>
       <Header>
         <Title>{state?.name || 'not found'}</Title>
       </Header>
-      {loading ? <Loader>loading</Loader> : null}
+      {loading ? (
+        <Loader>loading</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? 'Yes' : 'No'}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{price?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{price?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          <Tabs>
+            <Tab isActive={chartMatch !== null}>
+              <Link to='chart'>Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null}>
+              <Link to='price'>Price</Link>
+            </Tab>
+          </Tabs>
+
+          <Routes>
+            <Route path='chart' element={<Chart />} />
+            <Route path='price' element={<Price />} />
+          </Routes>
+        </>
+      )}
     </Container>
   );
 }
